e7410e77efa3faec1459f4d1d8772269643e1a94
Sanjay1712KSK <sanjayksk1712@gmail.com>
2025-03-11 20:36:45 +0530

    Updated

--- MatrixTransposition.c
+++ MatrixTransposition.c
@@ -1,17 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-    int a;
-    printf("Enter the size of the row in the array : ");
-    scanf("%d",&a);
-    int b;
-    printf("\nEnter the size of the col in the array : ");
-    scanf("%d",&b);
+void Transpose(int rows,int cols,int mat[rows][cols]){
+    int mattrans[cols][rows];
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            mattrans[j][i]=mat[i][j];
+        }
+    }
+    selection:
+    printf("Printing the elements of the array \n");
+    printf("If you want the array to be printed as same as the written format 1 or to print the values of the array in the detailed manner then 2 -->>");
+    int d;
+    scanf("%d",&d);
     printf("\n");
-    int mat[a][b];
+    if(d==1){
+        for(int i=0;i<cols;i++){
+            for(int j=0;j<rows;j++){
+                printf("%d ",mattrans[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    else if (d==2)
+    {
+        for(int i=0;i<cols;i++){
+            for(int j=0;j<rows;j++){
+                printf("The [%d] [%d] element is->> ",i,j);
+                printf("%d ",mattrans[i][j]);
+                printf("\n");
+            }
+        }
+    }
+    else{
+        goto selection;
+    }
+}
+int main(){
+    int rows;
+    printf("Enter the size of the row in the array : ");
+    scanf("%d",&rows);
+    int cols;
+    printf("\nEnter the size of the col in the array : ");
+    scanf("%d",&cols);
+    printf("\n");
+    int mat[rows][cols];
     printf("Enter the values into the array \n ");
-    for(int i=0;i<a;i++){
-        for(int j=0;j<b;j++){
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
             printf("Enter the [%d] [%d] element here ->> ",i,j);
             scanf("%d",&mat[i][j]);
             printf("\n");
@@ -24,8 +59,8 @@ int main(){
     scanf("%d",&d);
     printf("\n");
     if(d==1){
-        for(int i=0;i<a;i++){
-            for(int j=0;j<b;j++){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 printf("%d ",mat[i][j]);
             }
             printf("\n");
@@ -33,8 +68,8 @@ int main(){
     }
     else if (d==2)
     {
-        for(int i=0;i<a;i++){
-            for(int j=0;j<b;j++){
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
                 printf("The [%d] [%d] element is->> ",i,j);
                 printf("%d ",mat[i][j]);
                 printf("\n");
@@ -44,5 +79,6 @@ int main(){
     else{
         goto selection;
     }
+    Transpose(rows,cols,mat[rows][cols]);
     return 0;
 }
